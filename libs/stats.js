a9277e398dafe07dbc7841631964b85d0cdef160
gpuminingir <45966078+gpuminingir@users.noreply.github.com>
2019-04-10 14:24:35 +0100

    Update stats.js

--- libs/stats.js
+++ libs/stats.js
@@ -452,7 +452,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
                             shareCount: 0
                         };
 						
-						coinStats.blocks.lastblock = this.getBlocks;
+						coinStats.blocks.lastblock = getBlocks;
 						
                         for(var j = replies[i + 10].length; j > 0; j--){
                             var jsonObj;
